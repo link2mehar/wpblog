f29cba6b0db1d246cdd5f7633aeaa42939a9c893
Irfan Razzaq <31338182+link2mehar@users.noreply.github.com>
2018-01-06 02:24:37 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wp Blog</title>
+</head>
+<body>
+<div class="container">
+	<header>
+		<div class="header-logo"></div>
+		<div class="header-nav"></div>
+	</header>
+</div>
+</body>
+</html>
