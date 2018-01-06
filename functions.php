451f6e443c0a7b7510e0ddfaaca438044b695c0a
Irfan Razzaq <31338182+link2mehar@users.noreply.github.com>
2018-01-06 02:52:03 +0000

    update function.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 //====Wp blog Scripts and Styles
 
 function wp_blog_scripts() {
-	wp_enqueue_style( 'blog-style', get_stylesheet_uri());
+	wp_enqueue_style( 'blog-style', get_template_directory_uri() .'/style.css');
 }
 
 add_action( 'wp_enqueue_scripts', 'wp_blog_scripts' );  
