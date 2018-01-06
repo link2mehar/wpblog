2a538985f9ad9f61e23385305e9c0a53dd196191
Irfan Razzaq <31338182+link2mehar@users.noreply.github.com>
2018-01-06 02:27:31 +0000

    second commit

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
 		<div class="header-logo"></div>
 		<div class="header-nav"></div>
 	</header>
+	<div class="breadcrumbs">
+		
+	</div>
+	<div class="content-area">
+		<div class="site-content">
+			
+		</div>
+		<aside>
+			
+		</aside>
+	</div>
+	<footer>
+		
+	</footer>
 </div>
 </body>
 </html>
